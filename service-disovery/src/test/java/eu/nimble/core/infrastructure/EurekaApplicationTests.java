f9838dcce0199d2268ef17f824084bc0d90d9582
Johannes Innerbichler <johannes.innerbichler@salzburgresearch.at>
2016-10-18 09:45:56 +0200

    initialised project

---
+++ service-disovery/src/test/java/eu/nimble/core/infrastructure/EurekaApplicationTests.java
@@ -0,0 +1,15 @@
+package eu.nimble.core.infrastructure;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class EurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
