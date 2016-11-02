3d044ab948ce9df6703b6aedcb36bda83313e135
Johannes Innerbichler <johannes.innerbichler@salzburgresearch.at>
2016-11-02 19:30:01 +0100

    worked on proper statup in docker

--- gateway-proxy/src/main/docker/entry.sh
+++ gateway-proxy/src/main/docker/entry.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
-sleep 8
+echo "Stalling for Config Server"
+while true; do
+    nc -q 1 config-server 8888 2>/dev/null && break
+done
+
+echo "Stalling for Service Discovery"
+while true; do
+    nc -q 1 service-discovery 8761 2>/dev/null && break
+done
+
+echo "Starting Application"
 java -Djava.security.egd=file:/dev/./urandom -jar /app.jar
