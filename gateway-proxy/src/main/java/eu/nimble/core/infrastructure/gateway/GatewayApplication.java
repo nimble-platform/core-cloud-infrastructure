1c32bdbc0cbcbb8fc986c305a56846824586b1d4
Johannes Innerbichler <johannes.innerbichler@salzburgresearch.at>
2017-03-30 13:55:08 +0200

    worked on cors problem

--- gateway-proxy/src/main/java/eu/nimble/core/infrastructure/gateway/GatewayApplication.java
+++ gateway-proxy/src/main/java/eu/nimble/core/infrastructure/gateway/GatewayApplication.java
@@ -1,24 +1,17 @@
 package eu.nimble.core.infrastructure.gateway;
 
-import eu.nimble.core.infrastructure.gateway.filter.CustomCorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
+@ComponentScan
 public class GatewayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
-
-	@Bean
-	public CustomCorsFilter customCorsFilter() {
-		return new CustomCorsFilter();
-	}
-
 }
